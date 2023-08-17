69ca2e2773021952bb9db053e4665e96dc30e4d6
NPGgamer <npro22007008002@gmail.com>
2023-08-17 14:19:35 +0300

    Update S_One.cs

--- myGitTest2/Assets/S_One.cs
+++ myGitTest2/Assets/S_One.cs
@@ -7,7 +7,7 @@ public class S_One : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //test1
     }
 
     // Update is called once per frame
